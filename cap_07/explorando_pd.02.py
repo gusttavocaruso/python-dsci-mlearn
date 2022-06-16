e7137d15d35c61f4bbe4637cde6b56e6e8d656dc
gustacaru <gustavoalmeida26@hotmail.com>
2022-06-16 19:23:07 -0300

    update

--- cap_07/explorando_pd.02.py
+++ cap_07/explorando_pd.02.py
@@ -4,6 +4,7 @@ transport = pd.read_csv('./files/transportes.csv', index_col=0)
 
 print(transport)
 
+# Busca informações sobre Motocicleta
 print(transport.loc['Motocicleta'])
 
 # Excluir linhas 2, 4
@@ -14,3 +15,11 @@ print(transport.drop('Autonomia', axis = 1)) #metodo axis identifica que é uma 
 
 # Excluir duas colunas pelo nome
 print(transport.drop(['Peso', 'Consumo'], axis = 1))
+
+
+# DICA para excluir registros com caracteristica especifica (por ex: consumo zero e peso 450)
+autonomia_zero = transport.index[transport.Consumo == 0].tolist()
+peso_zero = transport.index[transport['Peso'] == 450].tolist()
+
+temp = autonomia_zero + peso_zero
+print(transport.drop(temp))
